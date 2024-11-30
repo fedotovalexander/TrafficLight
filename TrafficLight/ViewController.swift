6291629e8285e200e40408627af007a5b6e7065d
fedotovalexander <fedotov.sanek@gmail.com>
2024-11-30 12:33:07 +0300

    Complete app

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var trafficLightState = 0
 
+class ViewController: UIViewController {
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = redLightView.frame.height / 2
+        redLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        yellowLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+        greenLightView.alpha = 0.3
+        changeButton.layer.cornerRadius = 10
+        changeButton.backgroundColor = .blue
     }
 
+    @IBAction func changeLightButtonTapped(_ sender: UIButton) {
+                        
+        switch trafficLightState {
+        case 0:
+            redLightView.alpha = 1
+                greenLightView.alpha = 0.3
+                yellowLightView.alpha = 0.3
+                sender.setTitle("Next", for: .normal)
+        case 1:
+            yellowLightView.alpha = 1
+                greenLightView.alpha = 0.3
+                redLightView.alpha = 0.3
 
+            sender.setTitle("Next", for: .normal)
+        case 2:
+                greenLightView.alpha = 1
+                yellowLightView.alpha = 0.3
+                redLightView.alpha = 0.3
+                sender.setTitle("Next", for: .normal)
+        default:
+                greenLightView.alpha = 0.3
+                yellowLightView.alpha = 0.3
+                redLightView.alpha = 0.3
+                sender.setTitle("Start", for: .normal)
+        }
+        
+        trafficLightState =  (trafficLightState + 1 ) % 4
+
+    }
+    
 }
 
